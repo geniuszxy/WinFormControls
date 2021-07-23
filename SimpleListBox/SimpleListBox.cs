8a27ba73c0e0db882d21e6dd6939cad4340aa143
Shingo <geniusz2009-zxy@yahoo.com.cn>
2021-07-23 23:16:09 +0800

    SimpleListBox

--- SimpleListBox/SimpleListBox.cs
+++ SimpleListBox/SimpleListBox.cs
@@ -13,27 +13,40 @@ namespace WinFormControls
 {
 	public partial class SimpleListBox : UserControl
 	{
-		private List<ISimpleListBoxItem> _items;
+		/// <summary>
+		/// Height of each item
+		/// </summary>
+		public int ItemHeight { get; set; }
+
+		private ArrayList _items;
+		private int _selectIndex;
+		private int _topIndex;
 
 		public SimpleListBox()
 		{
-			_items = new List<ISimpleListBoxItem>();
+			_items = new ArrayList();
 			InitializeComponent();
 		}
 
-		public void AddItem(ISimpleListBoxItem item)
+		/// <summary>
+		/// Add an item
+		/// </summary>
+		public void AddItem(object item)
 		{
 			_items.Add(item);
 			Invalidate();
 		}
 
-		public void AddItems(IEnumerable<ISimpleListBoxItem> items)
+		/// <summary>
+		/// Add many items
+		/// </summary>
+		public void AddItems(ICollection items)
 		{
 			_items.AddRange(items);
 			Invalidate();
 		}
 
-		public void InsertItem(ISimpleListBoxItem item, int index)
+		public void InsertItem(object item, int index)
 		{
 			_items.Insert(index, item);
 			Invalidate();
@@ -51,7 +64,7 @@ namespace WinFormControls
 			Invalidate();
 		}
 
-		public ISimpleListBoxItem GetItemAt(int index)
+		public object GetItemAt(int index)
 		{
 			return _items[index];
 		}
@@ -65,42 +78,21 @@ namespace WinFormControls
 		{
 			base.OnPaint(e);
 
-
-			//// Create rectangle for clipping region.
-			//Rectangle clipRect = new Rectangle(0, 0, 100, 100);
-
-			//// Set clipping region of graphics to rectangle.
-			//e.Graphics.SetClip(clipRect);
-
-			//e.Graphics.FillRectangle(Brushes.Red, 0, 0, 1000, 1000);
-
-			//// Translate clipping region.
-			//int dx = 50;
-			//int dy = 50;
-			//e.Graphics.TranslateClip(dx, dy);
-
-			//// Fill rectangle to demonstrate translated clip region.
-			//e.Graphics.FillRectangle(new SolidBrush(Color.Black), 0, 0, 500, 300);
-
-
-
 			var g = e.Graphics;
+			var size = ClientSize;
 
-			g.SetClip(new Rectangle(0, 0, 100, 100));
+			//g.SetClip(new Rectangle(0, 0, 100, 100));
 			e.Graphics.FillRectangle(Brushes.Red, 0, 0, 1000, 1000);
 			float dy = 0f;
 
 
-			g.TranslateClip(50, 50);
-			//item.Draw(g);
-			g.FillRectangle(Brushes.Black, 0, 0, 99, 100);
-
-			//foreach (var item in _items)
-			//{
-			//	g.TranslateClip(50, 20);
-			//	//item.Draw(g);
-			//	g.FillRectangle(Brushes.Black, new Rectangle(0, 0, 10, 10));
-			//}
+			foreach (var item in _items)
+			{
+				g.FillRectangle(Brushes.Blue, new Rectangle(0, 0, 100, 150));
+				var text = item.ToString();
+				g.TranslateTransform(0, 20);
+				g.TranslateClip(0, 20);
+			}
 		}
 	}
 }
